a812c0ca5eb05b07faf5d35568ed5132746649ba
Berkay Karadag <berkay.karadag@outlook.com>
2022-12-06 12:05:00 +0100

    components implement

--- src/components/DecreaseCounter.js
+++ src/components/DecreaseCounter.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class DecreaseCounter extends Component {
   render() {
     return (
-      <div>DecreaseCounter</div>
+      <div></div>
     )
   }
 }
--- src/components/IncreaseByTwoCounter.js
+++ src/components/IncreaseByTwoCounter.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class IncreaseByTwoCounter extends Component {
   render() {
     return (
-      <div>IncreaseByTwoCounter</div>
+      <div></div>
     )
   }
 }
