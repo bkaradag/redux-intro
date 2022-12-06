c5d3d308e68fcd51381dd7e0de072bc87eec5f7d
Berkay Karadag <berkay.karadag@outlook.com>
2022-12-06 12:52:24 +0100

    decrease and bytwo counter implementiert

--- src/components/IncreaseByTwoCounter.js
+++ src/components/IncreaseByTwoCounter.js
@@ -1,9 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { increaseByTwoCounter } from "../redux/actions/counterActions";
 
-export default class IncreaseByTwoCounter extends Component {
+
+class IncreaseByTwoCounter extends Component {
   render() {
     return (
-      <div></div>
-    )
+      <div>
+        <button
+          onClick={(e) => {
+            this.props.dispatch(increaseByTwoCounter());
+          }}
+        >
+          {" "}
+          2 artir{" "}
+        </button>
+      </div>
+    );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(increaseByTwoCounter, dispatch) };
+}
+
+export default connect(mapDispatchToProps)(IncreaseByTwoCounter);
